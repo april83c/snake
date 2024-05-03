b764434e967ab8258d137942b80260a45af42bf7
April <april@dummy.cafe>
2024-05-03 07:34:40 +0200

    frontend-web: start making the different pages

--- packages/frontend-web/src/web.ts
+++ packages/frontend-web/src/web.ts
@@ -1,178 +1,71 @@
-import Snake, { SnakeState, type Vector2 } from '@april83c/snake';
+import WebSnake, { WebSnakeState } from './WebSnake';
 
-enum WebSnakeState {
-	Stopped = 0,
-	Paused = 1,
-	Running = 2
+function notNull<desired>(v: desired | null) {
+	if (v == null) throw new Error('Null');
+	else return v as desired;
 }
 
-class WebSnake {
-	state: WebSnakeState;
-
-	game: Snake;
-	inputBuffer: Vector2[];
-	minimumTickLength: number;
-	lastTickStarted: number;
-
-	mainView: HTMLCanvasElement;
-	mainViewContext: CanvasRenderingContext2D;
-	mainViewTileSize: number;
-	mainViewOffset: Vector2;
-
-	scoreElement: HTMLElement;
-
-	constructor(doc: Document, mainView: HTMLCanvasElement, score: HTMLElement) {
-		this.state = WebSnakeState.Running;
-		this.game = new Snake({ x: 30, y: 15 }, 5);
-		this.minimumTickLength = 16 * 4;
-		this.lastTickStarted = Date.now() - this.minimumTickLength;
-
-		// Set up main view
-		this.mainView = mainView;
-		const context: CanvasRenderingContext2D | null = this.mainView.getContext('2d');
-		if (context === null) {
-			throw new Error('Couldn\'t get main view canvas context.');
-		} else {
-			this.mainViewContext = context;
-		}
-
-		// Observe main view size
-		this.mainViewTileSize = 1;
-		this.mainViewOffset = { x: 0, y: 0 };
-
-		const resizeHandler = () => {
-			this.mainView.width = this.mainView.clientWidth;
-			this.mainView.height = this.mainView.clientHeight;
-
-			this.mainViewTileSize = Math.floor(Math.min(
-				this.mainView.width / this.game.boardSize.x,
-				this.mainView.height / this.game.boardSize.y
-			));
-			this.mainViewOffset.x = Math.floor((this.mainView.width - (this.mainViewTileSize * this.game.boardSize.x)) / 2);
-			this.mainViewOffset.y = Math.floor((this.mainView.height - (this.mainViewTileSize * this.game.boardSize.y)) / 2);
-
-			this.render();
-		}
-		resizeHandler();
-
-		const observer = new ResizeObserver(resizeHandler);
-		observer.observe(this.mainView);
-
-		// Listen for input
-		this.inputBuffer = [];
-		doc.addEventListener('keydown', (event) => {
-			let handled = false;
-
-			if (event.key !== undefined) {
-				switch(event.key) {
-					case 'ArrowLeft':
-						handled = true;
-						this.inputBuffer.push({ x: -1, y: 0 });
-						break;
-					case 'ArrowUp':
-						handled = true;
-						this.inputBuffer.push({ x: 0, y: -1 });
-						break;
-					case 'ArrowRight':
-						handled = true;
-						this.inputBuffer.push({ x: 1, y: 0 });
-						break;
-					case 'ArrowDown':
-						handled = true;
-						this.inputBuffer.push({ x: 0, y: 1 });
-						break;
-				}
-			} else if (event.keyCode !== undefined) {
-				switch(event.keyCode) {
-					case 37:
-						handled = true;
-						this.inputBuffer.push({ x: -1, y: 0 });
-						break;
-					case 38:
-						handled = true;
-						this.inputBuffer.push({ x: 0, y: -1 });
-						break;
-					case 39:
-						handled = true;
-						this.inputBuffer.push({ x: 1, y: 0 });
-						break;
-					case 40:
-						handled = true;
-						this.inputBuffer.push({ x: 0, y: 1 });
-						break;
-				}
-			}
-
-			if (handled) event.preventDefault();
-		})
-
-		this.scoreElement = score;
-
-		this.mainLoop();
-	}
-
-	mainLoop() {
-		const tickStarted = Date.now();
-		switch (this.state) {
-			case WebSnakeState.Stopped:
-				return;
-			case WebSnakeState.Running:
-				// Input and game logic
-				const framesToRun = Math.max(1, (tickStarted - this.lastTickStarted) / this.minimumTickLength);
-				for (let i = 1; i < framesToRun; i++) if (this.game.state == SnakeState.Running) this.game.tick(this.inputBuffer.shift());
-
-				// Rendering
-				this.render();
-
-				// Text
-				this.scoreElement.innerText = this.game.snake.length.toString();
-			case WebSnakeState.Paused:
-				// Next frame
-				this.lastTickStarted = tickStarted;
-				setTimeout(
-					() => { this.mainLoop() }, 
-					Math.max(0, this.minimumTickLength - (Date.now() - tickStarted))
-				);
-				break;
-		}
-	}
-
-	render() {
-		// Rendering (main view)
-		this.mainViewContext.clearRect(0, 0, this.mainView.width, this.mainView.height);
-		
-		// Background
-		this.mainViewContext.fillStyle = `#333333`;
-		this.mainViewContext.fillRect(this.mainViewOffset.x, this.mainViewOffset.y, this.mainViewTileSize * this.game.boardSize.x, this.mainViewTileSize * this.game.boardSize.y);
-
-		// Elements
-		for (let row = 0; row < this.game.boardSize.y; row++) {
-			for (let column = 0; column < this.game.boardSize.x; column++ ) {
-				const snakeIndex = this.game.snake.findIndex(snakePiece => snakePiece.x == column && snakePiece.y == row);
-				if (snakeIndex != -1) {
-					if (snakeIndex == 0) this.mainViewContext.fillStyle = 'green';
-					else this.mainViewContext.fillStyle = 'darkGreen';
-				} else if (this.game.apples.find(apple => apple.x == column && apple.y == row)) {
-					this.mainViewContext.fillStyle = 'red';
-				} else {
-					continue;
-				}
-
-				this.mainViewContext.fillRect(this.mainViewOffset.x + column * this.mainViewTileSize, this.mainViewOffset.y + row * this.mainViewTileSize, this.mainViewTileSize, this.mainViewTileSize);
-			}
-		}
-	}
-}
+// Main code
 
 let webSnake: WebSnake;
 
-function startSnake() {
-	const mainView = document.getElementById('mainView');
-	const score = document.getElementById('score');
-	if (
-		mainView instanceof HTMLCanvasElement
-		&& score instanceof HTMLElement
-	) webSnake = new WebSnake(document, mainView, score);
+enum Page {
+	Loading = 0,
+	Game = 1,
+	Setup = 2,
+	GameOver = 3
 }
 
-window.onload = startSnake;
+class Router {
+	activePage: Page; // TODO: How to do private setters in TS? Or maybe just replace go() with a setter
+	pages: {
+		[Page.Loading]: HTMLElement
+		[Page.Game]: HTMLElement;
+		[Page.Setup]: HTMLElement;
+		[Page.GameOver]: HTMLElement;
+	};
+
+	constructor() {
+		this.pages = {
+			[Page.Loading]: notNull(document.getElementById('page-loading')),
+			[Page.Game]: notNull(document.getElementById('page-game')),
+			[Page.Setup]: notNull(document.getElementById('page-setup')),
+			[Page.GameOver]: notNull(document.getElementById('page-gameover'))
+		};
+
+		this.activePage = Page.Loading;
+	}
+
+	go(page: Page) {
+		this.pages[this.activePage].style.display = 'none';
+		this.pages[page].style.display = '';
+		this.activePage = page;
+	}
+}
+
+function main() {
+	const router = new Router();
+	
+	// Buttons
+	const startButton = document.querySelector('#page-setup #start');
+	if (!(startButton instanceof HTMLButtonElement)) throw new Error('Error getting elements for setup');
+	startButton.addEventListener('click', () => {
+		router.go(Page.Game);
+		startSnake();
+	});
+
+	router.go(Page.Setup);
+}
+
+function startSnake() {
+	const mainView = document.querySelector('#page-game #mainView');
+	const score = document.querySelector('#page-game #score');
+	if (!(
+		mainView instanceof HTMLCanvasElement
+		&& score instanceof HTMLElement
+	)) throw new Error('Error getting elements for WebSnake');
+	
+	webSnake = new WebSnake(document, mainView, score)
+}
+
+window.onload = main;
